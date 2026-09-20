a1aca30dfa5cb1417185c57365deb41499442e4e
Kubi <jakub.wegrzyniak1239@gmail.com>
2026-09-20 18:15:31 +0200

    Add transaction frontend flow

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,120 +1,184 @@
-import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import './App.css'
+import { useEffect , useState } from "react"
+
+type TransactionSummary = {
+  totalIncome: number
+  totalExpenses: number
+  balance: number
+}
+
+type Transaction = {
+
+  id: number
+  amount: number
+  type: string
+  category: string
+  date: string
+  description: string
+}
+
+type TransactionForm = {
+  amount: number
+  type: string
+  category: string
+  date: string
+  description: string
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  console.log("APP RENDER")
+
+  const [summary, setSummary] = useState<TransactionSummary>({
+    totalIncome: 0,
+    totalExpenses: 0,
+    balance: 0
+  })
+
+  const [transactions, setTransactions] = useState<Transaction[]>([])
+
+  const [form, setForm] = useState<TransactionForm>({
+  amount: 0,
+  type: "EXPENSE",
+  category: "OTHER",
+  date: "",
+  description: ""
+})
+
+  const [transactionTypes, setTransactionTypes] = useState<string[]>([])
+
+  const [transactionCategories, setTransactionCategories] = useState<string[]>([])
+
+  const fetchSummary = () => {
+  fetch('http://localhost:8080/transactions/summary')
+    .then(response => response.json())
+    .then(data => setSummary(data))
+  }
+
+  const fetchTransactions = () => {
+  fetch('http://localhost:8080/transactions')
+    .then(response => response.json())
+    .then(data => setTransactions(data))
+  }
+
+  useEffect(() => {
+  fetchSummary()
+  fetchTransactions()
+  }, [])
+
+  useEffect(() => {
+    fetch('http://localhost:8080/transactions/types')
+    .then(response => response.json())
+    .then(data => setTransactionTypes(data))
+  }, [])
+
+  useEffect(() => {
+    fetch('http://localhost:8080/transactions/categories')
+    .then(response => response.json())
+    .then(data => setTransactionCategories(data))
+  }, [])
+  
+  
+  const handleSubmit = (e: React.FormEvent) => {
+  e.preventDefault()
+
+  fetch('http://localhost:8080/transactions', {
+    method: "POST",
+    body: JSON.stringify(form),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+    .then(response => {
+      if (response.ok) {
+        fetchTransactions()
+        fetchSummary()
+        setForm({
+          amount: 0,
+          type: "EXPENSE",
+          category: "OTHER",
+          date: "",
+          description: ""
+})
+      }
+    })
+  }
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
 
-      <div className="ticks"></div>
+        <h1> SmartWallet działa </h1> <br />
+        <h2> Summary: </h2>
+        <p>Income: {summary.totalIncome}</p>
+        <p>Expenses: {summary.totalExpenses}</p>
+        <p>Balance: {summary.balance}</p>
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+        <br />
+        <h2> Transactions: </h2>
+        {transactions.map(transaction => (
+            <div key={transaction.id}>
+                <p>Description: {transaction.description}</p>
+                <p>Amount: {transaction.amount}</p>
+                <p>Type: {transaction.type}</p>
+                <p>Category: {transaction.category}</p>
+                <p>Date: {transaction.date} </p>
+                <br />
+            </div>))}
+        
+            <form name="form1" onSubmit={handleSubmit}>
+              
+              <input
+                  type="number"
+                  value={form.amount}
+                  onChange={(e) => 
+                      setForm({...form,amount: Number(e.target.value)})
+                  }
+              />
+              <br />
+              <input 
+                  type="text" 
+                  value={form.description}
+                  onChange={(e) => 
+                    setForm({...form, description: String(e.target.value)})
+                  }
+              />
+              <br />
+              <input 
+                type="date"
+                value={form.date}
+                onChange={(e) =>
+                  setForm({...form, date: String(e.target.value)})
+                } 
+              />
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+              <br />
+              <select
+                value={form.type}
+                onChange={(e) => 
+                    setForm({...form, type: e.target.value
+                    })
+                }
+              >
+                {transactionTypes.map(type => (
+                  <option key={type} value={type}> {type} </option>
+                ))}
+              </select>
+              
+              <br />
+              <select
+                value={form.category}
+                onChange={(e) => 
+                    setForm({...form, category: e.target.value
+                    })
+                }
+              >
+                {transactionCategories.map(category => (
+                  <option key={category} value={category}> {category} </option>
+                ))}
+              </select>
+            <br />
+            <button type="submit">Add transaction</button>
+            
+            </form>
+
     </>
   )
 }
